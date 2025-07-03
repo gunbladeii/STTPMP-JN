c3e5cc1077c482eb05115b3e874a41d874b557f5
gunbladeii~fisha <53096103+gunbladeii@users.noreply.github.com>
2025-07-03 23:32:57 +0800

    Update script.js

--- script.js
+++ script.js
@@ -587,12 +587,12 @@ function initDataTableDesign(tableId) {
     // ✅ Tambah ni dalam loadDataUsers()
     document.querySelectorAll('.btn-delete').forEach(function (btn) {
       btn.addEventListener('click', function () {
-        const index = parseInt(btn.getAttribute('data-index')) + 2;
+        const row = parseInt(btn.getAttribute('data-index')) + 2;
         const email = btn.getAttribute('data-email');
         if (confirm(`Padam pengguna ini?\n\nE-mel: ${email}`)) {
           google.script.run.withSuccessHandler(() => {
             loadDataUsers();
-          }).deleteUser(index);
+          }).deleteUser(row);
         }
       });
     });
