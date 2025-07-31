6b8ae2d57f1e0ba9326bcacd2db079c6fb364dd9
gunbladeii~fisha <53096103+gunbladeii@users.noreply.github.com>
2025-07-31 08:35:58 +0800

    Update script.js

--- script.js
+++ script.js
@@ -672,25 +672,25 @@
      
       // Top 5 syor
       google.script.run.withSuccessHandler(function (data) {
-                const body = document.getElementById("top5SyorBody"); // Gunakan ID jadual tunggal anda
-                if (!body) {
-                  console.error("Elemen 'syorTableBody' tidak ditemui.");
-                  return;
-                }
-                body.innerHTML = "";
+              const body = document.getElementById("top5SyorBody"); 
+              if (!body) {
+                console.error("Elemen 'syorTableBody' tidak ditemui.");
+                return;
+              }
+              body.innerHTML = "";
 
-                if (data && data.length > 0) {
-                  data.forEach((item, idx) => {
-                    const row = document.createElement("tr");
-                    const badgeClass = item.DominantStatus === "Hijau" ? "bg-success" : item.DominantStatus === "Kuning" ? "bg-warning text-dark" : "bg-danger";
-                    row.innerHTML = `
-                      <td>${idx + 1}</td>
-                      <td>${item.Laporan}</td>
-                      <td>${item.ResponsibleUnits}</td> <td><span class="fw-bold">${item.SkorWajaran}</span> <span class="badge ${badgeClass}">${item.DominantStatus}</span></td>
-                    `;
-                    body.appendChild(row);
-                  });
-                } else {
+              if (data && data.length > 0) {
+                data.forEach((item, idx) => {
+                  const row = document.createElement("tr");
+                  const badgeClass = item.DominantStatus === "Hijau" ? "bg-success" : item.DominantStatus === "Kuning" ? "bg-warning text-dark" : "bg-danger";
+                  row.innerHTML = `
+                    <td>${idx + 1}</td>
+                    <td>${item.Laporan}</td>
+                    <td>${item.ResponsibleUnits}</td> <td><span class="fw-bold">${item.SkorWajaran}</span> <span class="badge ${badgeClass}">${item.DominantStatus}</span></td>
+                  `;
+                  body.appendChild(row);
+                });
+              } else {
                   const row = document.createElement("tr");
                   row.innerHTML = `<td colspan="4" class="text-center">Tiada data dijumpai mengikut kriteria anda.</td>`;
                   body.appendChild(row);
